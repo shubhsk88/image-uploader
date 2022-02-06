c94e3a6e5702d81864dfdfe895e450934898899a
Shubham <shubham88singh@gmail.com>
2022-02-06 07:04:38 +0530

    Adding the buggy code

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -3,23 +3,38 @@ import S3 from 'aws-sdk/clients/s3';
 
 const s3 = new S3();
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      const busboyInstance = new busboy({ headers: req.headers });
-      busboyInstance.on(
-        'file',
-        (fieldname, file, filename, encoding, mimetype) => {
-          const params = {
-            Bucket: process.env.BUCKET_NAME,
-            Key: filename,
-            Body: file,
-          };
-        }
-      );
-      const result = await s3.upload(params).promise();
-      res.status(200).json(result);
+      console.log('Headers', req.body);
+      const bb = busboy({ headers: req.headers });
+      
+      bb.on('file', async (name, file, info) => {
+
+        const { filename, encoding, mimeType } = info;
+        console.log(
+          `File [${name}]: filename: %j, encoding: %j, mimeType: %j`,
+          filename,
+          encoding,
+          mimeType
+        );
+        const params = {
+          Bucket: process.env.BUCKET_NAME,
+          Key: filename,
+          Body: file,
+        };
+        // res.json({ ok: true });
+
+        // const result = await s3.upload(params).promise();
+        // console.log({ result });
+        // res.status(200).json(result);
+      });
+      bb.end(req.rawBody);
+
+      // req.pipe(bb);
+      return res.json({ hello: true });
     } catch (error) {
+      console.log(error);
       res.status(500).json(error);
     }
   }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,39 @@
 import Head from 'next/head';
+import { useDropzone } from 'react-dropzone';
+import { useCallback } from 'react';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import { api } from '../utils';
 
 export default function Home() {
-  return <div className={styles.container}>Hello world</div>;
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+    const formData = new FormData();
+
+    formData.append('file', acceptedFiles[0]);
+
+    api
+      .post('/upload', formData, {
+        onUploadProgress: (progressEvent) => {
+          console.log(progressEvent.loaded / progressEvent.total);
+        },
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  return (
+    <div className={styles.container}>
+      Hello world
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag drop some files here, or click to select files</p>
+        )}
+      </div>
+    </div>
+  );
 }
